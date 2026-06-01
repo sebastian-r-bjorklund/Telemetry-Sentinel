cfdc97b10065d35a7ca3bef2b1b04831d98ceb88
sebastian-r-bjorklund <sebastian.r.bjorklund@gmail.com>
2026-06-01 13:12:44 +0200

    Add minimal standalone embassy STM32F407 firmware scaffold

---
+++ blinky.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+    info!("Telemetry Sentinel: online");
+
+    // PD12 = green LED (LD4) on the STM32F407G-DISC1
+    let mut led = Output::new(p.PD12, Level::High, Speed::Low);
+
+    loop {
+        led.set_high();
+        Timer::after_millis(300).await;
+        led.set_low();
+        Timer::after_millis(300).await;
+    }
+}
